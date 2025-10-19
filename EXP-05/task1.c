a4dfd9ce2c978ed81aa6fdc183dfed670c59ded7
MOHD ASHFAQUE MOHD HAROON SHAIKH <mhdashfaque@mhssce.ac.in>
2025-10-19 16:03:20 +0530

    Add files via upload

---
+++ EXP-05/task1.c
@@ -0,0 +1,45 @@
+/*
+    Expt 5 - Task 1
+    Module 2 : Iterative Function & Recursive Function
+    Aim: WAP to find the factorial of a number using iterative function.
+    Name: SHAIKH MOHD ASHFAQUE
+    BRANCH:
+    ROLL-NO:
+    DIV:
+    Subject:
+
+*/
+
+#include<stdio.h>
+
+int factorialIterative(int num){
+    int prod = 1;
+    for(int i=1; i <= num; i++){
+        prod *= i;
+    }
+
+    return prod;
+}
+
+
+int main(){
+    int num;
+
+    printf("\t\t\t *** Iterative Factorial Function *** \n\n");
+
+    printf("Enter Number:");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d", num, factorialIterative(num));
+
+    return 0;
+}
+
+/* OUTPUT
+
+                        *** Iterative Factorial Function ***
+
+Enter Number:5
+Factorial of 5 is 120
+
+*/
---
+++ EXP-05/task2.c
@@ -0,0 +1,46 @@
+/*
+    Expt 5 - Task 2
+    Module 2 : Functions and Parameter
+    Aim: WAP to find the factorial of a number using recursive function.
+
+    Name: SHAIKH MOHD ASHFAQUE
+    BRANCH:
+    ROLL-NO:
+    DIV:
+    Year:
+    Branch:
+
+*/
+
+#include<stdio.h>
+
+int factorialRecursive(int num){
+    if(num <= 1){
+        return 1;
+    }
+
+    return num * factorialRecursive(num-1);
+}
+
+
+int main(){
+    int num;
+
+    printf("\t\t\t *** Recursive Factorial Function *** \n\n");
+
+    printf("Enter Number:");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d", num, factorialRecursive(num));
+
+    return 0;
+}
+
+/* OUTPUT
+
+                         *** Recursive Factorial Function ***
+
+Enter Number:5
+Factorial of 5 is 120
+
+*/
