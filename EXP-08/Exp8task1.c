8e482b22f336647feacd9fef9a109d70233c2571
MOHD ASHFAQUE MOHD HAROON SHAIKH <mhdashfaque@mhssce.ac.in>
2025-11-05 22:26:09 +0530

    Add files via upload

---
+++ EXP-08/Exp8task1.c
@@ -0,0 +1,42 @@
+/*
+    Expt 8 - Task 1
+    Module 2 : Strings
+    Aim: WAP to find the length of a string without
+        using library function.
+         NAME: SHAIKH MOHD ASHFAQUE
+        BRANCH:
+        DIV:
+        SUBJECT: C PROGRAMMING
+        ROLL-NO:
+
+*/
+
+#include<stdio.h>
+
+
+int main(){
+    char str[100];
+    int length = 0;
+
+    printf("\t\t *** String Length Finder *** \n\n");
+
+    printf("Enter Your String:");
+    gets(str);
+
+    while(str[length] != '\0'){
+        length++;
+    }
+
+    printf("String Length: %d Charachters", length);
+
+    return 0;
+}
+
+/* OUTPUT
+
+                 *** String Length Finder ***
+
+Enter Your String:hello world
+String Length: 11 Charachters
+
+*/
---
+++ EXP-08/Exp8task2.c
@@ -0,0 +1,47 @@
+/*
+    Expt 8 - Task 2
+    Module 2 : Strings
+    Aim: WAP to check if the entered string is palindrome or not.
+    NAME: SHAIKH MOHD ASHFAQUE
+    BRANCH:
+    DIV:
+    SUBJECT: C PROGRAMMING
+    ROLL-NO:
+
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+
+int main(){
+    char str[100];
+    int i;
+
+    printf("\t\t *** Palindrome Finder *** \n\n");
+
+    printf("Enter Your String:");
+    gets(str);
+
+    // 012345678
+    // malayalam
+    for(i = 0; i < strlen(str) - 1; i++){
+        if(str[i] != str[strlen(str) - 1 - i]){
+            printf("The String is NOT a Palindrome");
+            return 0;
+        }
+    }
+
+    printf("The String is a Palindrome");
+
+    return 0;
+}
+
+/* OUTPUT
+
+                 *** Palindrome Finder ***
+
+Enter Your String:malayalam
+The String is a Palindrome
+
+*/
